bb39a659e2d29a631f4a26b9a1660b01e6b76c8e
Harshit Sharma <123459218+HsAhRaSrHmIaT@users.noreply.github.com>
2024-09-19 07:46:55 +0530

    Add Strikethrough style after completing a task.

--- WEB.py
+++ WEB.py
@@ -12,12 +12,20 @@ st.title("To-Do List App")
 st.subheader("This app will help you manage your tasks.")
 st.write("Your tasks:")
 
+import time
+
 for index, task in enumerate(tasks):
-    checkbox = st.checkbox(task, key=task)
-    if checkbox:
+    if task in st.session_state and st.session_state[task]:
+        st.markdown(f'<s style="color:grey">{task}</s>', unsafe_allow_html=True)
+        time.sleep(0.5)
         tasks.pop(index)
         functions.writeFile(tasks)
         del st.session_state[task]
         st.rerun()
+    else:
+        checkbox = st.checkbox(task, key=task)
+        if checkbox:
+            st.session_state[task] = True
+            st.rerun()
 
 st.text_input("", key="task", placeholder="Enter your task here", on_change=add_task)
